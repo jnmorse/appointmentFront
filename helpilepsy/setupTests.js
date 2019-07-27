7fdce83f7444535d6f4deac870ec458dee014711
Joseph Morse <tamed.lionheart@gmail.com>
2019-07-27 14:05:30 -0400

    test: setup node-fetch for testing

--- helpilepsy/setupTests.js
+++ helpilepsy/setupTests.js
@@ -1,4 +1,8 @@
+/* eslint-env node */
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import fetch from "node-fetch";
 
 Enzyme.configure({ adapter: new Adapter() });
+
+global.fetch = fetch;
